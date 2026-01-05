8377ea46830964c51e2f0ff851f092223ed441e2
Brooktie <b_frogge@coloradocollege.edu>
2026-01-04 22:50:54 -0700

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Portfolio from "../pages/Portfolio.vue";
-import Contact from "../pages/Contact.vue";
+import StudentWork from "../pages/Contact.vue"; // (rename later if you actually have StudentWork.vue)
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/portfolio", name: "Portfolio", component: Portfolio },
-  { path: "/contact", name: "Contact", component: Contact },
+  { path: "/", name: "home", component: Home },
+  { path: "/portfolio", name: "portfolio", component: Portfolio },
+  { path: "/student-work", name: "student-work", component: StudentWork },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 
 export default router;
+
