fb82bdcd21c1ddf54292392d76b4c9187e405f2a
Brooktie <b_frogge@coloradocollege.edu>
2026-01-04 22:40:49 -0700

    Fix SPA routing for Render

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Portfolio from "../pages/Portfolio.vue";
-import StudentWork from "../pages/Contact.vue";
+import Contact from "../pages/Contact.vue";
 
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  { path: "/portfolio", name: "Portfolio", component: Portfolio },
+  { path: "/contact", name: "Contact", component: Contact },
+];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: "/", name: "home", component: Home },
-    { path: "/portfolio", name: "portfolio", component: Portfolio },
-    { path: "/student-work", name: "student-work", component: StudentWork },
-  ],
-  scrollBehavior() {
-    return { top: 0 };
-  },
+  routes,
 });
+
+export default router;
